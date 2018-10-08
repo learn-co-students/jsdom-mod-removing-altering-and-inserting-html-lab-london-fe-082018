7be6f0b9610a9fe23d479dab6a66852b9f10aa7d
Donna Cadiente <djanecad@hotmail.com>
2018-10-08 21:54:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 /* Enter the code to remove the main node element under this comment */
 
+main.remove()
+
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+var element = document.createElement('newHeader')
+document.body.appendChild(element)
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute = ("id", "Victory");
+newHeader.innerText = "Donna is the Champion!";
+
+element.appendChild(newHeader);
+
+
